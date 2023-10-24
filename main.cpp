26ed7c0285277adfba59814143af7edbaf9eff81
HelpMe11122 <66992514+HelpMe11122@users.noreply.github.com>
2023-10-24 22:45:04 +0000

    init

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <unistd.h>
+
+
+using namespace std;
+
+
+void generate_passkey() {
+
+}
+
+void main() { // the first event, calling of a calling 
+    cout << "Welcome to realism banking - Entirely made in C++!";
+    usleep(10)
+}
